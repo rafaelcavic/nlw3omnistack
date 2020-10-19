935cc6064d2efdc518fa22a55400ec617e9438ac
unknown <rafaelcavic@hotmail.com>
2020-10-19 20:24:45 -0300

    Primeira Aula

---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import Landing from './pages/Landing/index';
+import OrphanagesMap from './pages/OrphanagesMap/index';
+
+function Routes(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Landing}/>
+                <Route path="/app" component={OrphanagesMap}/>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+export default Routes;
